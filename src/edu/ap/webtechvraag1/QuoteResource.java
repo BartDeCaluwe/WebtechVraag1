12c27d7d0280d89aae1763e351619bc144825bb1
BartDeCaluwe <de.caluwe.bart@gmail.com>
2016-04-12 18:36:57 +0200

    POST Method
    Added post method.

---
+++ src/edu/ap/webtechvraag1/QuoteResource.java
@@ -0,0 +1,58 @@
+package edu.ap.webtechvraag1;
+
+import java.io.*;
+import java.util.HashSet;
+import java.util.Map;
+
+import javax.ws.rs.*;
+import javax.json.*;
+import redis.clients.jedis.*;
+
+@Path("/quotes")
+public class QuoteResource {
+	
+	Jedis jedis;
+	
+	@GET
+	@Produces({"text/html"})
+	public String getQuotes() {
+		String htmlString = "<html><body>";
+		try {
+			jedis = new Jedis("localhost");
+			
+			Map<String, String> hset = jedis.hgetAll("quotes");
+			
+			for(int i = 0; i < hset.size(); i++){
+				htmlString += "<br>Author : " + hset.get("author") + "</br><br>";
+				htmlString += "Quote : " + hset.get("quote") + "<br>";
+				htmlString += "<br><br>";
+			}
+		} catch (Exception e) {
+			htmlString = e.getMessage();
+		}
+		return htmlString + "</body></html>";
+	}	
+	
+	@POST
+	@Path("{author}")
+	@Produces({"text/html"})
+	public String getQuotesAuthor(@PathParam("author") String author) {
+		String htmlString = "<html><body>";
+		try {
+			jedis = new Jedis("localhost");
+	        
+			Map<String, String> hset = jedis.hgetAll("quotes");
+			for(int i = 0; i < hset.size(); i++){
+				if(hset.get(author) == author){
+					htmlString += "Quote : " + hset.get("quote") + "<br>";
+					htmlString += "<br><br>";
+				}
+			}
+		} 
+		catch (Exception ex) {
+			htmlString = ex.getMessage();
+		}
+		
+		return htmlString;
+	}
+}
